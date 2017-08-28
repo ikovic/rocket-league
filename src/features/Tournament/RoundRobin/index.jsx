8e8b29d2c07e0fa5f73391da4b79ea5fb1406f86
Ivan Kovic <ivan.kovic@seekandhit.com>
2017-08-28 13:03:22 +0200

    Working round robin

--- src/features/Tournament/RoundRobin/index.jsx
+++ src/features/Tournament/RoundRobin/index.jsx
@@ -2,9 +2,35 @@ import React, { PureComponent } from 'react';
 import { connect } from 'react-redux';
 
 class RoundRobin extends PureComponent {
+  renderPairs(round) {
+    console.log(round);
+    return round.pairs.map((pair, idx) =>
+      <li key={idx}>
+        {pair[0][0].name} VS {pair[0][1].name}
+      </li>
+    );
+  }
+
+  renderRounds() {
+    const rounds = this.props.rounds;
+
+    return (
+      <li>
+        {rounds.map(round =>
+          <ul key={round.id}>
+            {this.renderPairs(round)}
+          </ul>
+        )}
+      </li>
+    );
+  }
+
   render() {
-    console.dir(this.props.rounds);
-    return <h1>ROUND ROBIN</h1>;
+    return (
+      <ul>
+        {this.renderRounds()}
+      </ul>
+    );
   }
 }
 
